9d2088bdeba362d1b83fe9a65184bc7a10e1381e
Olivier Chotin <olivier.chotin@ocia.fr>
2022-06-14 17:40:07 +0200

    Signed-off-by: Olivier Chotin <olivier.chotin@ocia.fr>

--- app.py
+++ app.py
@@ -86,11 +86,15 @@ async def messages(req: Request) -> Response:
     return Response(status=HTTPStatus.OK)
 
 
-APP = web.Application(middlewares=[bot_telemetry_middleware, aiohttp_error_middleware])
-APP.router.add_post("/api/messages", messages)
+def init_func(argv) :
+    APP = web.Application(middlewares=[bot_telemetry_middleware, aiohttp_error_middleware])
+    APP.router.add_post("/api/messages", messages)
+    return APP
 
 if __name__ == "__main__":
+    APP = init_func(None)
+	
     try:
-        web.run_app(APP, host="localhost", port=CONFIG.PORT)
+        web.run_app(APP, host="0.0.0.0", port=CONFIG.PORT)
     except Exception as error:
         raise error
